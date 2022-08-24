cafa5337e6433622aaf89215f579eaf42ec0c964
Dmitrii Dorofeev <djudman@gmail.com>
2022-08-24 14:02:26 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='yandex_cloud_s3',
+    version='0.0.1',
+    packages=['yandex_cloud_s3'],
+    author='Dmitrii Dorofeev',
+    author_email='djudman@gmail.com',
+    # url='https://djud.site/projects/yandex-cloud-s3'
+)
